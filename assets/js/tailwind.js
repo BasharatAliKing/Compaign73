9c7f946e0ae0786e0154dfb22b2a7187e7d12900
Basharat Ali <basharatalidogar14@gmail.com>
2023-12-25 00:19:43 -0800

    first commit

---
+++ assets/js/tailwind.js
@@ -0,0 +1,20 @@
+tailwind.config = {
+    theme: {
+      extend: {
+        colors: {
+          clifford: '#da373d',
+        },
+        container:{
+            center:true,
+            padding: {
+                DEFAULT: '1rem',
+                sm: '2rem',
+                lg: '2rem',
+                xl: '2rem',
+                '2xl': '6rem',
+              },
+        },
+
+      }
+    }
+  }
